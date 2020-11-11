ca4a5a29f8d3c07008a28bd350eef47cd712c88b
MaheshKumarGM-tech <73214700+MaheshKumarGM-tech@users.noreply.github.com>
2020-11-11 12:29:53 +0530

    Revert "Update app.js"
    This reverts commit 6d55a28310fdb8ce50d6d668a3bd5adbd53e6dd8.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express()
 
-app.get('/', (req, res)=> res.send('Hello World this is mahesh testing 123, Welcome Back....again'));
+app.get('/', (req, res)=> res.send('Simple tesrt.....'));
 
 const portn = process.env.port || 3000;
 app.listen(portn, ()=>{
