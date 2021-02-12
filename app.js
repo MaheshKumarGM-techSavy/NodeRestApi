7d6c126998aa812f7d7d8883131ae6f6b0f9f265
MaheshKumarGM-tech <73214700+MaheshKumarGM-tech@users.noreply.github.com>
2021-02-12 12:49:55 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express()
 
-app.get('/', (req, res)=> res.send('Simple tesrt.....'));
+app.get('/', (req, res)=> res.send('Simple test.....'));
 
 const portn = process.env.port || 3000;
 app.listen(portn, ()=>{
