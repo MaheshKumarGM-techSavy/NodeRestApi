16cb4a80d63d9b606d011bda19a87a3c642bb434
MaheshKumarGM-tech <73214700+MaheshKumarGM-tech@users.noreply.github.com>
2021-02-12 13:02:45 +0530

    testing again

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express()
 
-app.get('/', (req, res)=> res.send('Simple test.....'));
+app.get('/', (req, res)=> res.send('Simple test update.....'));
 
 const portn = process.env.port || 3000;
 app.listen(portn, ()=>{
